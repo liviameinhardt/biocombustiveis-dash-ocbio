849de78652dae8aa58a14bfcd2df9c6fdddb9927
Livia <livia.meinhardt@gmail.com>
2025-04-30 11:15:18 -0300

    add custom theme

--- Resumo.py
+++ Resumo.py
@@ -4,7 +4,7 @@ from utils import millify
 
 #### Configs ####
 
-
+st.logo('logo.png', icon_image='logo.png',size='large')
 st.set_page_config(page_title="Descarbonização da Matriz de Combustíveis", page_icon=":bar_chart:", layout="wide")
 
 st.write(
